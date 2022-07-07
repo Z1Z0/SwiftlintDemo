347f52ad33fd732b35e1524ca203ab8527690ab8
Ahmed Abd El-Aziz <ahmedabdelaziz500@gmail.com>
2022-07-07 16:50:21 +0200

    finished the feature

---
+++ SwiftlintDemo/CreateNote.swift
@@ -0,0 +1,14 @@
+//
+//  CreateNote.swift
+//  SwiftlintDemo
+//
+//  Created by Ahmed Abd Elaziz on 07/07/2022.
+//
+
+import Foundation
+
+class CreateNote {
+    
+    var testNote = "created"
+    
+}
